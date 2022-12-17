376d913f02ccbfe437dc78ee2142e13ee522e9b5
Tommy <unconfigured@null.spigotmc.org>
2022-12-17 23:39:46 +0000

    added tailwind to carry the CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,31 @@
 <script>
-  import "../app.css";
+  import "../app.postcss";
   import KalenaIcon from "$lib/KalenaIcon.svelte";
   import NavBar from "$lib/NavBar.svelte";
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
 </script>
 
 <main class="container">
-  <KalenaIcon></KalenaIcon>
-  <NavBar></NavBar>
+  <!--<KalenaIcon></KalenaIcon>-->
+  <Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+      <img
+        src="https://flowbite.com/docs/images/logo.svg"
+        class="mr-3 h-6 sm:h-9"
+        alt="Flowbite Logo"
+      />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        Flowbite
+      </span>
+    </NavBrand>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden}>
+      <NavLi href="/" active={true}>Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/services">Services</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+      <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+  </Navbar>
   <slot />
 </main>
